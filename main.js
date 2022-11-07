cdc740a576e04245e8a1290c39d1faa96207458d
Felipe <felipelgsouza1@gmail.com>
2022-11-07 13:04:52 -0300

    Dia 5 - Full Group H Games

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function createGame(player1, hour, player2) {
   return `
     <li>
-      <img src="./assets/icon-${player1}.svg" alt="${player1}">
+      <img class="icon-size" src="./assets/icon-${player1}.svg" alt="${player1}">
       <strong>${hour}</strong>
-      <img src="./assets/icon-${player2}.svg" alt="${player2}">
+      <img class="icon-size" src="./assets/icon-${player2}.svg" alt="${player2}">
     </li>
   `
 }
@@ -23,6 +23,6 @@ function createCard(date, day, games) {
 
 document.querySelector("#cards").innerHTML =
 
-    createCard("24/11", "quinta", createGame("switzerland", "07:00", "cameroon") + createGame("brazil", "16:00", "serbia")) +
-    createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland")) +
-    createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+    createCard("24/11", "quinta", createGame("switzerland", "07:00", "cameroon") + createGame("uruguay", "10:00", "southkorea") + createGame("portugal", "13:00", "ghana") + createGame("brazil", "16:00", "serbia")) +
+    createCard("28/11", "segunda", createGame("cameroon", "07:00", "serbia") + createGame("southkorea", "10:00", "ghana") + createGame("brazil", "13:00", "switzerland") + createGame("portugal", "16:00", "uruguay")) +
+    createCard("02/12", "sexta", createGame("southkorea", "12:00", "portugal") + createGame("ghana", "12:00", "uruguay") + createGame("serbia", "16:00", "switzerland") + createGame("brazil", "16:00", "cameroon"))
